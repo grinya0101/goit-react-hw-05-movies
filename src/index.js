72f6ae8e2342cac2559bc54da0fff2440749b6ae
grinya0101 <grinya1994015@gmail.com>
2022-10-19 00:23:21 +0300

    add

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       {/* для гита нужно с basename */}
-      <BrowserRouter basename="/goit-react-hw-05-movies/">
-        {/* <BrowserRouter> */}
+      {/* <BrowserRouter basename="/goit-react-hw-05-movies/"> */}
+        <BrowserRouter>
         <App />
       </BrowserRouter>
     </ThemeProvider>
